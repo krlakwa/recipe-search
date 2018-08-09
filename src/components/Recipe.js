c7629a59086c2bfffc58fc6f9ce614f4c848cc2c
Krzysztof Łakwa <fenrir14@gmail.com>
2018-08-10 01:07:12 +0200

    Add styles

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 const Recipe = props => {
  return (
-  <div>
-    <img src={props.recipe.image_url} alt={props.recipe.title} />
-    <p>{ props.recipe.title }</p>
+  <div className="col-md-4">
+    <div className="recipes__box">
+      <img
+        className="recipes__img"
+        src={props.recipe.image_url}
+        alt={props.recipe.title}
+      />
+      <div className="recipe__text">
+        <h5 className="recipes__title">{ props.recipe.title }</h5>
+        <p className="recipes__subtitle">
+          Publisher:
+            <a
+              className="recipes__link"
+              href={props.recipe.publisher_url}
+              target="_blank"> { props.recipe.publisher }
+            </a>
+        </p>
+      </div>
+    </div>
   </div>
  )
 }
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Recipe from './Recipe';
 
 const Recipes = props => (
-  <div>
-  { props.recipes.map(recipe => <Recipe key={recipe.recipe_id} recipe={recipe} />) }
+  <div className="container">
+    <div className="row">
+      {props.recipes.map(recipe => <Recipe key={recipe.recipe_id} recipe={recipe} />)}
+    </div>
   </div>
 )
 
 export default Recipes;
-
-
-/**/
