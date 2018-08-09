3e65d793fb00a60347ca05f37498cdd3f1d37031
Krzysztof Łakwa <fenrir14@gmail.com>
2018-08-09 22:24:09 +0200

    Add getRecipe method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './components/Form';
 
+const API_KEY = 'e88bb1852406385e07de8a7e908f42fe';
 class App extends Component {
+
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const prefix = 'https://cors-anywhere.herokuapp.com/';
+    const api_call = await fetch(`${prefix}http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
       </div>
     );
   }
