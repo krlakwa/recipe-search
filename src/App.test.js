43bf21d26ffb8589216629a359d9408b53230c30
Krzysztof Łakwa <fenrir14@gmail.com>
2018-08-22 13:44:07 +0200

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('should have a form element', () => {
+  const app = mount(<App />);
+  expect(app.find('form').length).toBe(1);
 });
